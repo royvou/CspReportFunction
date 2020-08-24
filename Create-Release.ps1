9f6e3e2b9d64f3870fe4f1ab1fe159487db8f674
Roy <royvou@hotmailcom>
2020-08-24 20:50:05 +0200

    Fix line endings

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -1,4 +1,4 @@
-Push-Location .\src
+Push-Location ./src
 
 $releaseFolder = "..\release"
 $releaseFoldertmp = "$releaseFolder\tmp"
@@ -8,7 +8,7 @@ if (Test-Path $releaseFolder) { Remove-Item $releaseFolder -Force -Recurse }
 
 # Build Section
 dotnet publish cspreportfunction.sln -c Release -f netcoreapp3.1 --nologo -o $releaseFoldertmp #-r linux-x64
-Compress-Archive -Path "$releaseFoldertmp\*" -DestinationPath "$releaseFolder\$zipArchive" -force
+Compress-Archive -Path "$releaseFoldertmp/*" -DestinationPath "$releaseFolder/$zipArchive" -force
 remove-item -Force -Recurse $releaseFoldertmp
 
 # Test Section
